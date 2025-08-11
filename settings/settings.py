fe12a8495bc73e8ff589b72fcd4dacb71923bbd5
Viktor <vityachishevich@gmail.com>
2025-08-11 22:09:46 +0200

    up

--- settings/settings.py
+++ settings/settings.py
@@ -140,9 +140,8 @@ SPECTACULAR_SETTINGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False  # Отключите TLS
-EMAIL_USE_SSL = True   # Включите SSL
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 ADMIN_EMAIL = config('ADMIN_EMAIL', default='')
